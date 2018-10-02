6f87b8d0ea7b7d978b82a66d95cd4bf9173a443d
ajmetal <ajmetal@live.com>
2018-10-02 13:58:14 -0700

    improvements to adjacency function and dijkstras

--- p1.py
+++ p1.py
@@ -18,23 +18,24 @@ def dijkstras_shortest_path(initial_position, destination, graph, adj):
 
     """
     queue = [initial_position]
+    visitedFrom = {}
     while queue:
         current_node = queue[0]
         if current_node == destination:
-            return path_to(destination, graph)
+            return path_to(destination, graph, visitedFrom)
         else:
-            for new in adj(current_node, graph):
-                if new.prev == None:
-                    new.prev = current_node
+            for new in adj(graph, current_node):
+                if new not in visitedFrom:
+                    visitedFrom[new] = current_node
                     queue.insert(0, new)
     return(-1)
 
-def path_to(destination, graph):
+def path_to(destination, graph, visitedFrom):
     path = []
     current_node = destination
-    while current_node.prev:
-        path.insert(0, current_node.prev)
-        current_node = current_node.prev
+    while current_node in visitedFrom:
+        path.insert(0, visitedFrom[current_node])
+        current_node = visitedFrom[current_node]
     return path
 
 
@@ -90,40 +91,45 @@ def navigation_edges(level, cell):
         if(dimY < wall[1]):
             dimY = wall[1]
 
+    dimX -= 1
+    dimY -= 1
+
     adjList = ()
+    x = cell[0]
+    y = cell[1]
+
     #top-left
-    if cell[0] > 1 and cell[1] > 1:
-        adjCell = level['spaces'][(cell[0] - 1, cell[1] - 1)]
-        adjList += ((adjCell[0][0], adjCell[0][1]), adjCell[1])
+    if x > 1 and y > 1:
+        adjCell = (x -1, y - 1)
+        adjList += (adjCell, level['spaces'][adjCell])
     #above
-    if cell[1] > 1:
-        adjCell = level['spaces'][(cell[0],cell[1] - 1)]
-        adjList += ((adjCell[0][0], adjCell[0][1]), adjCell[1])
+    if y > 1:
+        adjCell = (x, y - 1)
+        adjList += (adjCell, level['spaces'][adjCell])
     #top-right
-    if cell[0] < dimX and cell[1] > 1:
-        adjCell = level['spaces'][(cell[0] + 1, cell[1] - 1)]
-        adjList += ((adjCell[0][0], adjCell[0][1]), adjCell[1])
+    if x < dimX and y > 1:
+        adjCell = (x + 1, y - 1)
+        adjList += (adjCell, level['spaces'][adjCell])
     #right
-    if cell[0] < dimX:
-        adjCell = level['spaces'][(cell[0] + 1, cell[1])]
-        print(adjCell)
-        adjList += ((adjCell[0][0], adjCell[0][1]), adjCell[1])
+    if x < dimX:
+        adjCell = (x + 1, y)
+        adjList += (adjCell, level['spaces'][adjCell])
     #bottom-right
-    if cell[1] < dimY and cell[0] < dimX:
-        adjCell = level['spaces'][(cell[0] + 1, cell[1] + 1)]
-        adjList += ((adjCell[0][0], adjCell[0][1]), adjCell[1])
+    if y < dimY and x < dimX:
+        adjCell = (x + 1, y + 1)
+        adjList += (adjCell, level['spaces'][adjCell])
     #below
-    if cell[1] < dimY:
-        adjCell = level['spaces'][(cell[0],cell[1] + 1)]
-        adjList += ((adjCell[0][0], adjCell[0][1]), adjCell[1])
+    if y < dimY:
+        adjCell = (x, y + 1)
+        adjList += (adjCell, level['spaces'][adjCell])
     #bottom-left
-    if cell[1] < dimY and cell[0] > 1 :
-        adjCell = level['spaces'][(cell[0] - 1, cell[1] + 1)]
-        adjList += ((adjCell[0][0], adjCell[0][1]), adjCell[1])
+    if y < dimY and x > 1:
+        adjCell = (x - 1, y + 1)
+        adjList += (adjCell, level['spaces'][adjCell])
     #left
-    if cell[0] > 1:
-        adjCell =level['spaces'][(cell[0] - 1, cell[1])]
-        adjList += ((adjCell[0][0], adjCell[0][1]), adjCell[1])
+    if x > 1:
+        adjCell = (x - 1, y)
+        adjList += (adjCell, level['spaces'][adjCell])
    
     return adjList
     
